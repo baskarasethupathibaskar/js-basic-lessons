73be8377c5c7d088aeb80c58c58488f7e5367fc1
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-03-01 20:02:58 +0530

    js

--- src/sample2.js
+++ src/sample2.js
@@ -1,14 +1,18 @@
-//wrong
-const     q    =       document.querySelector(    'p'     );
-//correct
+/* eslint-disable semi */
+// wrong
+const q = document.querySelector('p');
+// correct
 const p = document.querySelector('p');
 /*
  *  Using a variable without declaring the variable
  */
-//wrong
+// wrong
 // town = "chennai"
 // console.log(town);
 
-const city = `Chennai`;
+const city = 'Chennai';
 console.log(city);
 const age = 100;
+console.log(q);
+console.log(p);
+console.log(age);
---
+++ src/sample4.js
@@ -0,0 +1,19 @@
+/* eslint-disable semi */
+console.log('Hi Node!!!');
+function callme () {
+  const x = 100;
+  const y = x * 2;
+
+  if (x < 500) {
+    console.log('x < 500');
+    const z = x + y;
+    console.log(`z : ${z}`);
+  }
+  if (y < 500) {
+    console.log('y < 500');
+    // const v1 = z + y;
+    // console.log(`V1:${v1}`);
+  }
+}
+
+console.log(callme());
