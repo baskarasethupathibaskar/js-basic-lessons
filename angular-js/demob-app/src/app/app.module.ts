58e9e5b8a1799a23c6a68f9420d8216619631c97
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-04-04 09:52:31 +0530

    include commit

--- angular-js/demob-app/src/app/app.module.ts
+++ angular-js/demob-app/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
+import {  FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent,FormComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
